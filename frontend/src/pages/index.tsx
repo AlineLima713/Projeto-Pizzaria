5cb9c7a46d86152a07eeb62c2fdfe78eae8812f6
Aline Lima <alinelima713@gmail.com>
2023-05-28 17:24:59 -0300

    config folders

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>Aline & Vinicius S2</h1>
+      </div>
+    </>
+  )
+}
