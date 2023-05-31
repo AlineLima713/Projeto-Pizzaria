b669d885fb9b664e575b1a4853332a954d393bc7
Aline Lima <alinelima713@gmail.com>
2023-05-31 20:14:16 -0300

    created login screen

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head"
+import Image from "next/image";
+import styles from '../../styles/home.module.scss'
+
+import logoImg from '../../public/logo.svg';
+
+import { Input } from "../components/ui/Input";
 
 export default function Home() {
   return (
@@ -6,8 +12,22 @@ export default function Home() {
       <Head>
         <title>SujeitoPizza - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Aline & Vinicius S2</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+            />
+
+            <Input
+              placeholder="Sua senha"
+              type="password"
+            />
+          </form>
+        </div>
       </div>
     </>
   )
