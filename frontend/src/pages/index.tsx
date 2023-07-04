2e5612acd5ef77693ffc45fabbc187a7d59cccca
Aline Lima <alinelima713@gmail.com>
2023-07-03 21:04:43 -0300

    private routes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,6 +14,8 @@ import { AuthContext } from '../contexts/AuthContext';
 
 import Link from "next/link";
 
+import { canSSRGuest } from '../utils/canSSRGuest';
+
 export default function Home() {
   const { signIn } = useContext(AuthContext)
 
@@ -84,3 +86,10 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+
+  return {
+    props: {}
+  }
+})
