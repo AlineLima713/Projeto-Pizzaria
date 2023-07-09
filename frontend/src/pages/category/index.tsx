67b843994d5f1df8b154ae5bfb502254f7958f27
Aline Lima <alinelima713@gmail.com>
2023-07-09 12:04:42 -0300

    created product page

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -3,13 +3,28 @@ import Head from "next/head";
 import { Header } from "../../components/Header";
 import styles from './styles.module.scss'
 
+import { setupAPIClient } from "../../services/api";
+import { toast } from "react-toastify";
+
+import { canSSRAuth } from "../../utils/canSSRAuth";
+
 export default function Category() {
   const [name, setName] = useState('')
 
   async function handleRegister(event: FormEvent) {
     event.preventDefault();
 
-    alert("CATEGORIA " + name)
+    if (name === '') {
+      return;
+    }
+
+    const apiClient = setupAPIClient();
+    await apiClient.post('/category', {
+      name: name
+    })
+
+    toast.success('Categoria cadastrada com sucesso!')
+    setName('');
   }
 
   return (
@@ -36,8 +51,16 @@ export default function Category() {
             </button>
 
           </form>
+
         </main>
       </div>
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+})
