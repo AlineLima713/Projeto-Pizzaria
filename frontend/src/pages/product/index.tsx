efbd3cf325e87298bdf3c3e720e53a13ed243249
Aline Lima <alinelima713@gmail.com>
2023-07-12 19:15:55 -0300

    looking for categories

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -7,10 +7,24 @@ import { canSSRAuth } from "../../utils/canSSRAuth";
 
 import { FiUpload } from "react-icons/fi";
 
-export default function Product() {
+import { setupAPIClient } from "../../services/api";
+
+type ItemProps = {
+  id: string;
+  name: string;
+}
+
+interface CategoryProps {
+  categoryList: ItemProps[];
+}
+
+export default function Product({ categoryList }: CategoryProps) {
 
   const [avatarUrl, setAvatarUrl] = useState('');
   const [imageAvatar, setImageAvatar] = useState(null);
+
+  const [categories, setCategories] = useState(categoryList || [])
+  const [categorySelected, setCategorySelected] = useState(0)
 
   function handleFile(e: ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) {
@@ -27,6 +41,11 @@ export default function Product() {
       setImageAvatar(image);
       setAvatarUrl(URL.createObjectURL(e.target.files[0]))
     }
+  }
+
+  //Quando voce seleciona uma nova categoria na lista
+  function handleChangeCategory(event) {
+    setCategorySelected(event.target.value)
   }
 
   return (
@@ -63,13 +82,14 @@ export default function Product() {
 
             </label>
 
-            <select>
-              <option>
-                Bebida
-              </option>
-              <option>
-                Pizzas
-              </option>
+            <select value={categorySelected} onChange={handleChangeCategory}>
+              {categories.map((item, index) => {
+                return (
+                  <option key={item.id} value={index}>
+                    {item.name}
+                  </option>
+                )
+              })}
             </select>
 
             <input
@@ -101,7 +121,13 @@ export default function Product() {
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get('/category');
+
   return {
-    props: {}
+    props: {
+      categoryList: response.data
+    }
   }
 })
