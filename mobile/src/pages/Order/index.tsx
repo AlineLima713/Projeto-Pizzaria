a75879120c2f845c016b60b3981dd513260a066a
Aline Lima <alinelima713@gmail.com>
2023-09-05 20:46:41 -0300

    searching category

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 
 import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
@@ -14,11 +14,32 @@ type RouteDetailParams = {
   }
 }
 
+type CategoryProps = {
+  id: string;
+  name: string;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
   const route = useRoute<OrderRouteProps>();
   const navigation = useNavigation();
+
+  const [category, setCategory] = useState<CategoryProps[] | []>([]);
+  const [categorySelected, setCategorySelected] = useState<CategoryProps>();
+
+  const [amount, setAmount] = useState('1');
+
+  useEffect(() => {
+    async function loadInfo() {
+      const response = await api.get('/category')
+
+      setCategory(response.data);
+      setCategorySelected(response.data[0])
+    }
+
+    loadInfo();
+  }, [])
 
   async function handleCloseOrder() {
     try {
@@ -44,9 +65,13 @@ export default function Order() {
         </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.input}>
-        <Text style={{ color: '#FFF' }}>Pizzas</Text>
-      </TouchableOpacity>
+      {category.length !== 0(
+        <TouchableOpacity style={styles.input}>
+          <Text style={{ color: '#FFF' }}>
+            {categorySelected?.name}
+          </Text>
+        </TouchableOpacity>
+      )}
 
       <TouchableOpacity style={styles.input}>
         <Text style={{ color: '#FFF' }}>Pizza de calabresa</Text>
@@ -58,7 +83,8 @@ export default function Order() {
           style={[styles.input, { width: '60%', textAlign: 'center' }]}
           placeholderTextColor="#F0F0F0"
           keyboardType="numeric"
-          value="1"
+          value={amount}
+          onChangeText={setAmount}
         />
       </View>
 
